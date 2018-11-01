2b72d07c4950a546190a2f403a9b3a29ea870fa8
Nishant <Nishant>
2018-11-01 08:41:16 -0400

    Implemented Manager Module and icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http'
 import { NgModule } from '@angular/core'
 import { FlexLayoutModule } from '@angular/flex-layout'
 import { BrowserModule } from '@angular/platform-browser'
@@ -6,7 +7,6 @@ import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { HomeComponent } from './home/home.component'
 import { InventoryModule } from './inventory/inventory.module'
-import { ManagerModule } from './manager/manager.module'
 import { MaterialModule } from './material.module'
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
 import { PosModule } from './pos/pos.module'
@@ -18,12 +18,12 @@ import { UserModule } from './user/user.module'
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    ManagerModule,
     FlexLayoutModule,
     InventoryModule,
     MaterialModule,
     PosModule,
     UserModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
